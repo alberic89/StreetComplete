c23b23fdc9fff3a55983fff7ec20dfdb85c6ea2d
alberic89 <alberic89@gmx.com>
2024-12-15 18:39:21 +0100

    Update InternetConnectionState.kt

--- app/src/main/java/de/westnordost/streetcomplete/data/platform/InternetConnectionState.kt
+++ app/src/main/java/de/westnordost/streetcomplete/data/platform/InternetConnectionState.kt
@@ -7,7 +7,7 @@ import android.net.Network
 import android.net.NetworkCapabilities
 import android.net.NetworkRequest
 import androidx.core.content.getSystemService
-//import de.westnordost.streetcomplete.data.preferences.Preferences
+import de.westnordost.streetcomplete.data.preferences.Preferences
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -32,11 +32,11 @@ class InternetConnectionState(context: Context) {
         awaitClose { connectivityManager.unregisterNetworkCallback(networkCallback) }
     }
 
-    //private val prefs by inject<Preferences>()
-    //private val ignoreConnectionState: Boolean get() = prefs.ignoreConnectionState
+    private val prefs by inject<Preferences>()
+    private val ignoreConnectionState: Boolean get() = prefs.ignoreConnectionState
 
-    val isConnected: Boolean get() = true
-        //connectivityManager.activeNetworkInfo?.isConnected == true //|| ignoreConnectionState
+    val isConnected: Boolean get() =
+        connectivityManager.activeNetworkInfo?.isConnected == true || ignoreConnectionState
 
     private val connectivityManager = context.getSystemService<ConnectivityManager>()!!
 }
